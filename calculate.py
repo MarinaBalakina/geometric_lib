1cc0465307fc244888a63805eb731d0117c43721
MarinaBalakina <marinabalakina132@gmail.com>
2024-11-16 21:29:48 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,33 +1,43 @@
 import circle
 import square
 
-
 figs = ['circle', 'square']
 funcs = ['perimeter', 'area']
-sizes = {}
+sizes = {
+		"perimeter-circle": 1,
+		"area-circle": 1,
+		"perimeter-square": 1,
+		"area-square": 1,
+}
 
 def calc(fig, func, size):
-	assert fig in figs
-	assert func in funcs
+		assert fig in figs, f"Figure {fig} is not supported. Available figures: {figs}"
+		assert func in funcs, f"Function {func} is not supported. Available functions: {funcs}"
 
-	result = eval(f'{fig}.{func}(*{size})')
-	print(f'{func} of {fig} is {result}')
+		result = eval(f'{fig}.{func}(*{size})')
+
+		if func == 'perimeter':
+				operation_str = f"Perimeter of {fig} ({' + '.join(map(str, size))}) = {result}"
+		elif func == 'area':
+				operation_str = f"Area of {fig} (π * {size[0]}^2) = {result}" if fig == 'circle' else f"Area of {fig} ({size[0]} * {size[0]}) = {result}"
+		else:
+				operation_str = f"{func} of {fig} with size {', '.join(map(str, size))} = {result}"
+
+		return operation_str
 
 if __name__ == "__main__":
-	func = ''
-	fig = ''
-	size = list()
-    
-	while fig not in figs:
-		fig = input(f"Enter figure name, avaliable are {figs}:\n")
-	
-	while func not in funcs:
-		func = input(f"Enter function name, avaliable are {funcs}:\n")
-	
-	while len(size) != sizes.get(f"{func}-{fig}", 1):
-		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
-	
-	calc(fig, func, size)
+		func = ''
+		fig = ''
+		size = []
 
+		while fig not in figs:
+				fig = input(f"Enter figure name, available are {figs}:\n")
 
+		while func not in funcs:
+				func = input(f"Enter function name, available are {funcs}:\n")
 
+		while len(size) != sizes.get(f"{func}-{fig}", 1):
+				size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
+
+		operation_str = calc(fig, func, size)
+		print(operation_str)
