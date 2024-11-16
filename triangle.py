36fcf32aa8f10b6a7b45d345897bd9f0e9dcd22a
MarinaBalakina <marinabalakina132@gmail.com>
2024-11-16 21:32:12 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,6 +1,23 @@
+import math
+
+def is_number(value):
+    if not isinstance(value, (int, float)):
+        raise ValueError("Input must be a number")
+    if value < 0:
+        raise ValueError("Input must be greater than or equal to 0")
+
 def area(a, b, c):
-    return (a + b + c) / 2
+    '''Принимает число a, b, c, возвращает половину сумму a, b, c'''
+    is_number(a)
+    is_number(b)
+    is_number(c)
+    p = (a+b+c)/2
+    return math.sqrt(p*(p-a)*(p-b)*(p-c))
 
 
 def perimeter(a, b, c):
+    '''Принимает число a, b, c, возвращает сумму a, b, c'''
+    is_number(a)
+    is_number(b)
+    is_number(c)
     return a + b + c
