89670b9cbbc2f477a17ec6ad2ee5a4f4adfd5509
MarinaBalakina <marinabalakina132@gmail.com>
2024-11-17 18:20:45 +0300

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -7,16 +7,18 @@ def is_number(value):
         raise ValueError("Input must be greater than or equal to 0")
 
 def area(a, b, c):
-    '''Принимает число a, b, c, возвращает половину сумму a, b, c'''
+    '''Возвращает площадь треугольника по формуле Герона'''
     is_number(a)
     is_number(b)
     is_number(c)
-    p = (a+b+c)/2
-    return math.sqrt(p*(p-a)*(p-b)*(p-c))
-
+    p = (a + b + c) / 2
+    area_squared = p * (p - a) * (p - b) * (p - c)
+    if area_squared <= 0:
+        raise ValueError("The provided sides do not form a valid triangle.")
+    return math.sqrt(area_squared)
 
 def perimeter(a, b, c):
-    '''Принимает число a, b, c, возвращает сумму a, b, c'''
+    '''Возвращает периметр треугольника'''
     is_number(a)
     is_number(b)
     is_number(c)
