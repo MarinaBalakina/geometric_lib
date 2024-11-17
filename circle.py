69ba0f0299949a3b27fc9591b988736c2ea0ec57
MarinaBalakina <marinabalakina132@gmail.com>
2024-11-17 18:18:55 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,10 +1,11 @@
 import math
+
 def is_number(value):
-     if not isinstance(value, (int, float)):
-         raise ValueError("Input must be a number")
-     if value < 0:
-         raise ValueError("Input must be greater than or equal to 0")
-         
+    if not isinstance(value, (int, float)):
+        raise ValueError("Input must be a number")
+    if value < 0:
+        raise ValueError("Input must be greater than or equal to 0")
+
 def area(r):
     is_number(r)
     return math.pi * r * r
